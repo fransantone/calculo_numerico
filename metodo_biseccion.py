97270d24bbfa4d4177f629b03f165b89edbf8dcb
francesco <santonefrancesco@gmail.com>
2025-08-25 17:36:47 -0300

    Comentarios Biseccion

--- metodo_biseccion.py
+++ metodo_biseccion.py
@@ -1,22 +1,28 @@
+# Importamos las librerias math y sympy para utilizar la nomenclatura tecnica de matematica y que se entienda al escribir en consola y realizar las cuentas
 from math import *
 import sympy as sp
 
 def biseccion():
+    # Definimos la variable X como un simbolo matematico para poder utilizar a futuro
     x = sp.symbols('x')
     func_str = input("Ingrese la función en x: ")
     try:
+        # Habilitamos utilizar terminologia especifica somo "seno" y demas para que se interprete desde la consola y asi realizar calculos a futuro
         f_expr = sp.sympify(func_str,locals={"exp": sp.exp, "log": sp.log, "sqrt": sp.sqrt,"sin": sp.sin, "cos": sp.cos, "tan": sp.tan,"pi": sp.pi, "E": sp.E, "e": sp.E})
     except Exception as e:
         print(f"Función inválida: {e}")
         return
+    # Convertimos la funcion simbolica en una funcion que sea interpretada por python
     f = sp.lambdify(x, f_expr, "math")
     num = lambda msg: float(sp.sympify(msg, locals={"pi": sp.pi, "E": sp.E, "e": sp.E, "sqrt": sp.sqrt}))
+    # Solicitamos los valores de x0, x1 y tolerancia
     a = num(input("Ingrese el valor de A: "))
     b = num(input("Ingrese el valor de B: "))
     tol = float(sp.sympify(input("Ingrese el valor de la tolerancia: ")))
     m1 = a
     m = b
     k = 0
+    # Realizamos las cuentas para cada intervalo y mostramos por consola los intervalos con las raices
     if (f(a)*f(b)>0):
         print("La funcion no cambia de signo")
     while(abs(m1-m)>tol):
@@ -43,6 +49,7 @@ def iteraciones():
     print(f"Cantidad de iteraciones necesarias = {cantidad_iteraciones}")
     return 
 
+# Ejecutamos el codigo con un menu para calcular la raiz aproximada de una funcion o la cantidad de iteraciones necesarias
 if __name__ == "__main__":
     while True:
         while True:
