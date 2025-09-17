2e53f8681dc9a941c3840f44eac26890b8d170fd
francesco <santonefrancesco@gmail.com>
2025-09-17 16:59:05 -0300

    calculo del trapecio sin el error en base n

--- metodo_trapecio.py
+++ metodo_trapecio.py
@@ -11,7 +11,7 @@ def nodos(a, n, h):
     return xs
 
 def integral_trapecio (f, a, b, n, h):
-    xs = nodos(a, b, n)   
+    xs = nodos(a, n, h)   
     # esto seria la parte del trapecio que suma f(x0) + f(xn)
     s = f(xs[0]) + f(xs[-1])
     # esto seria la parte de 2 * la sumatoria de f(xi) hasta f(xn-1)
@@ -24,22 +24,24 @@ def error_trapecio_en_base_n():
     pass
 
 def main():
-    a = float(input("Ingrese el valor de A: "))
-    b = float(input("Ingrese el valor de B: "))
+    a = int(input("Ingrese el valor de A: "))
+    b = int(input("Ingrese el valor de B: "))
     n = int(input("Ingrese el valor de n: "))
-    funcion = input("Ingrese f(x) a evaluar")
+    funcion = input("Ingrese f(x) a evaluar: ")
     try:
+        x = sp.Symbol('x')
         # Habilitamos utilizar terminologia especifica somo "seno" y demas para que se interprete desde la consola y asi realizar calculos a futuro
         f_expr = sp.sympify(funcion,locals={"exp": sp.exp, "log": sp.log, "sqrt": sp.sqrt,"sin": sp.sin, "cos": sp.cos, "tan": sp.tan,"pi": sp.pi, "E": sp.E, "e": sp.E})
+        f_num = sp.lambdify(x, f_expr, "math")
     except Exception as e:
         print(f"Función inválida: {e}")
         return
     h = (b-a)/n
-    resultado_trapecio = integral_trapecio(f_expr, a, b, n, h)
+    resultado_trapecio = integral_trapecio(f_num, a, b, n, h)
     print("======= Resultados =======")
     print (f"Intervalo: {a} ; {b} | con {n} subintervalos")
     print(f"Integral en f(x): {resultado_trapecio}")
-    print(f"Error con {n} subintervalos: {error}")
+    #print(f"Error con {n} subintervalos: {error}")
 
 if __name__ == "__main__":
-    pass
+    main()
