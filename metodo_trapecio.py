03ab4ecc6f1e37d207bb1dff5398ce7e1ebbe54f
francesco <santonefrancesco@gmail.com>
2025-09-17 16:48:11 -0300

    Metodo del trapecio sin calculo del error

--- metodo_trapecio.py
+++ metodo_trapecio.py
@@ -1,4 +1,5 @@
 import math
+import sympy as sp
 
 def nodos(a, n, h):
     if n <= 0:
@@ -10,14 +11,35 @@ def nodos(a, n, h):
     return xs
 
 def integral_trapecio (f, a, b, n, h):
-    xs = nodos(a, b, n) 
-    
-    return
+    xs = nodos(a, b, n)   
+    # esto seria la parte del trapecio que suma f(x0) + f(xn)
+    s = f(xs[0]) + f(xs[-1])
+    # esto seria la parte de 2 * la sumatoria de f(xi) hasta f(xn-1)
+    for i in range (1,n):
+        s = s + 2 * f(xs[i])
+    resultado_final = (h/2) * s
+    return resultado_final
 
-if __name__ == "__main__":
-    a = int(input("Ingrese el valor de A: "))
-    b = int(input("Ingrese el valor de B: "))
+def error_trapecio_en_base_n():
+    pass
+
+def main():
+    a = float(input("Ingrese el valor de A: "))
+    b = float(input("Ingrese el valor de B: "))
     n = int(input("Ingrese el valor de n: "))
     funcion = input("Ingrese f(x) a evaluar")
+    try:
+        # Habilitamos utilizar terminologia especifica somo "seno" y demas para que se interprete desde la consola y asi realizar calculos a futuro
+        f_expr = sp.sympify(funcion,locals={"exp": sp.exp, "log": sp.log, "sqrt": sp.sqrt,"sin": sp.sin, "cos": sp.cos, "tan": sp.tan,"pi": sp.pi, "E": sp.E, "e": sp.E})
+    except Exception as e:
+        print(f"Función inválida: {e}")
+        return
     h = (b-a)/n
+    resultado_trapecio = integral_trapecio(f_expr, a, b, n, h)
+    print("======= Resultados =======")
+    print (f"Intervalo: {a} ; {b} | con {n} subintervalos")
+    print(f"Integral en f(x): {resultado_trapecio}")
+    print(f"Error con {n} subintervalos: {error}")
+
+if __name__ == "__main__":
     pass
