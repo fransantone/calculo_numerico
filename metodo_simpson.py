d07f369dcd383f81e5a41d035ce950d470c923fc
francesco <santonefrancesco@gmail.com>
2025-09-20 18:18:39 -0300

    metodo simspon cpmpuesto con n mayor a 2, no como esta en el burden y faires, sino otra forma un poco mas facil

---
+++ metodo_simpson.py
@@ -0,0 +1,46 @@
+import sympy as sp
+
+def nodos(a, n, h):
+    if n <= 0 or n % 2 != 0:
+        raise ValueError("n debe ser positiva y par")
+    xs = []
+    for i in range (n+1):
+        xi = a + i * h
+        xs.append(xi)
+    return xs
+
+def integral_simpson (f, a, b, n, h):
+    xs = nodos(a, n, h)
+    simpson = f(xs[0]) + f(xs[-1])
+    for i in range (1,n):
+        simpson = simpson + 2 * f(xs[i])
+    xm = []
+    for i in range (0,n):
+        xmi = (xs[i] + xs[i+1]) / 2
+        xm.append(xmi) 
+    for i in range (0, n):
+        simpson = simpson + 4 * f(xm[i])
+    resultado_final = (h/6) * simpson
+    return resultado_final
+
+def main():
+    a = float(input("Ingrese el valor de A: "))
+    b = float(input("Ingrese el valor de B: "))
+    n = int(input("Ingrese el valor de N: "))
+    funcion = input("Ingrese f(x) a evaluar: ")
+    try:
+        x = sp.Symbol('x')
+# Habilitamos utilizar terminologia especifica somo "seno" y demas para que se interprete desde la consola y asi realizar calculos a futuro
+        f_expr = sp.sympify(funcion,locals={"exp": sp.exp, "log": sp.log, "sqrt": sp.sqrt,"sin": sp.sin, "cos": sp.cos, "tan": sp.tan,"pi": sp.pi, "E": sp.E, "e": sp.E})
+        f_num = sp.lambdify(x, f_expr, "math")
+    except Exception as e:
+        print(f"Función inválida: {e}")
+        return
+    h = (b-a)/n
+    smp = integral_simpson(f_num, a, b, n, h)
+    print("======= Resultados =======")
+    print (f"Intervalo: {a} ; {b} | con {n} subintervalos")
+    print(f"Integral de f(x) por simpson: {smp}")
+
+if __name__ == "__main__":
+    main()
